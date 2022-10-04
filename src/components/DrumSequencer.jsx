bd3b743d7fef3441cf4a2a4e8d98c22a63b38f27
miguellacorte <76566384+miguellacorte@users.noreply.github.com>
2022-10-04 14:06:16 +0200

    loopers synced

--- src/components/DrumSequencer.jsx
+++ src/components/DrumSequencer.jsx
@@ -1,51 +1,50 @@
-import React from 'react'
+import React from "react";
 import * as Tone from "tone";
 import Sketch from "react-p5";
 
 const synth1 = new Tone.Synth().toDestination();
 const synth2 = new Tone.Synth().toDestination();
 
-let kickBaseURL = "/drumSamples/kickSamples"
-
-// let kickBaseURL = "https://github.com/miguellacorte/rythm_sequencer_resources/blob/082ab1d80861a2154cee9d77dd09ab74171d8963/drumSamples/kickSamples/"
+let kickBaseURL = "sounds/";
 
 const kickPlayer = new Tone.Players({
-  kick1: kickBaseURL + "808_kick.mp3",
-  kick2: kickBaseURL + "Ouija_Kick.mp3"
-
+  kick1: kickBaseURL + "kick1.mp3",
+  kick2: kickBaseURL + "kick2.mp3",
 }).toDestination();
 
-const now = Tone.now()
+const now = Tone.now();
 
-Tone.Transport.bpm.value = 120
+Tone.Transport.bpm.value = 120;
 
-const loopA = new Tone.Loop(time => {
-	Tone.loaded().then(() => {
-    kickPlayer.player('kick1').start();
+const drumLoopA = new Tone.Loop((time) => {
+  Tone.loaded().then(() => {
+    kickPlayer.player("kick2").start(time);
   });
-}, "4n").start(0);
+}, "8n").start(0.05); 
 
-Tone.Transport.start()
+const drumLoopB = new Tone.Loop((time) => {
+  Tone.loaded().then(() => {
+    kickPlayer.player("kick1").start(time);
+  });
+}, "2n").start(0.05);
+
+Tone.Transport.start();
 
 const play = () => {
-  console.log('play')
+  console.log("play");
   Tone.start();
 };
 
 export default function DrumSequencer() {
-
-   console.log('hello')
+  console.log(Tone.context.currentTime);
 
   return (
     <div>
-    <h1>drum sequencer!!</h1>
+      <h1>drum sequencer!!</h1>
 
-    <button 
-      onClick={play} 
-      className="playBtn"
-    >▶</button>
-  
-
+      <button onClick={play} className="playBtn">
+        ▶
+      </button>
     </div>
-  )
+  );
 }
