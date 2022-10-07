26acce9b90ba3e2d164046b7411d53e6f108a937
miguellacorte <76566384+miguellacorte@users.noreply.github.com>
2022-10-07 21:29:57 +0200

    added interaction parameters

--- src/components/DrumSequencer.jsx
+++ src/components/DrumSequencer.jsx
@@ -6,10 +6,13 @@ import { useState } from "react";
 export default function DrumSequencer() {
   console.log(Tone.context.currentTime);
 
-  const [rangeval, setRangeval] = useState(null);
+  const [slider1val, setSlider1val] = useState(null);
+  const [slider2val, setSlider2val] = useState(null);
+  const [slider3val, setSlider3val] = useState(2);
+  const [slider4val, setSlider4val] = useState(null);
+  const [slider5val, setSlider5val] = useState(null);
 
   let setup = (p5, canvasParentRef) => {
-    
     let canvas = p5
       .createCanvas(p5.windowWidth, p5.windowHeight)
       .parent(canvasParentRef);
@@ -19,6 +22,9 @@ export default function DrumSequencer() {
 
   let bpm = (Tone.Transport.bpm.value = 120);
 
+  let rythmDiv = ["16n", "8n", "5n", "4n", "3n", "2n"];
+  console.log(slider2val);
+
   const synth1 = new Tone.Synth().toDestination();
   const synth2 = new Tone.Synth().toDestination();
 
@@ -27,52 +33,108 @@ export default function DrumSequencer() {
   const kickPlayer = new Tone.Players({
     kick1: kickBaseURL + "kick1.mp3",
     kick2: kickBaseURL + "kick2.mp3",
+    kick3: kickBaseURL + "kick3.mp3",
+    kick4: kickBaseURL + "kick4.mp3",
+  }).toDestination();
+
+  const snarePlayer = new Tone.Players({
+    snare1: kickBaseURL + "snare1.mp3",
+    snare2: kickBaseURL + "snare2.mp3",
+    snare3: kickBaseURL + "snare3.mp3",
+    snare4: kickBaseURL + "snare4.mp3",
+    snare5: kickBaseURL + "snare5.mp3",
   }).toDestination();
 
   let draw = (p5) => {};
 
   const drumLoopA = new Tone.Loop((time) => {
     Tone.loaded().then(() => {
-      kickPlayer.player("kick2").start(time);
+      kickPlayer.player(`kick${slider3val}`).start(time);
     });
-  }, "8n").start(0.05);
+  }, rythmDiv[slider2val]).start(0);
 
   const drumLoopB = new Tone.Loop((time) => {
     Tone.loaded().then(() => {
-      kickPlayer.player("kick1").start(time);
+      snarePlayer.player(`snare${slider4val}`).start(time);
     });
-  }, "2n").start(0.05);
+  }, rythmDiv[slider5val]).start(0);
 
-  Tone.Transport.start();
-  Tone.Destination.volume.value = rangeval;
+  
+  Tone.Destination.volume.value = slider1val;
 
   const play = () => {
     console.log("play");
     Tone.start();
+    Tone.Transport.start();
   };
 
   return (
     <div>
       <h1>drum sequencer!!</h1>
 
-      <button onClick={play} className="playBtn">
-        ▶
-      </button>
-      <p>current tempo: {bpm}</p>
       <div>
-        {" "}
-        <Sketch setup={setup} draw={draw} />
-      </div>
+        <button onClick={play} className="playBtn">
+          ▶
+        </button>
 
-      <div>
+        <p>current tempo: {bpm}</p>
+
+        <h4>volume: {slider1val}</h4>
         <input
           type="range"
           className="custom-range"
           min="-60"
-          max="-15"
-          onClick={(event) => setRangeval(event.target.value)}
+          max="-18"
+          onClick={(event) => setSlider1val(event.target.value)}
         />
-        <h4>The range value is {rangeval}</h4>
+
+        <div>
+          <h4>Kick rythm division select: {slider2val}</h4>
+          <input
+            type="range"
+            className="custom-range"
+            min="0"
+            max="5"
+            onClick={(event) => setSlider2val(event.target.value)}
+          />
+        </div>
+
+        <div>
+          <h4>Kick select: {slider3val}</h4>
+          <input
+            type="range"
+            className="custom-range"
+            min="1"
+            max="4"
+            onClick={(event) => setSlider3val(event.target.value)}
+          />
+        </div>
+
+        <div>
+          <h4>Snare select: {slider4val}</h4>
+          <input
+            type="range"
+            className="custom-range"
+            min="1"
+            max="4"
+            onClick={(event) => setSlider4val(event.target.value)}
+          />
+        </div>
+
+        <div>
+          <h4>Snare rythm division select: {slider5val}</h4>
+          <input
+            type="range"
+            className="custom-range"
+            min="1"
+            max="4"
+            onClick={(event) => setSlider5val(event.target.value)}
+          />
+        </div>
+      </div>
+
+      <div>
+        <Sketch setup={setup} draw={draw} />
       </div>
     </div>
   );
